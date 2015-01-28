3e32aa1baabf77ffccfc5dcdf950c3e33a435cb9
Achim Botz <achim.botz@teradata.com>
2015-01-28 14:31:28 +0100

    added Timeout

--- src/main/scala/org/sandbox/akka/broadcast/PayloadBroadcast.scala
+++ src/main/scala/org/sandbox/akka/broadcast/PayloadBroadcast.scala
@@ -11,6 +11,8 @@ import akka.event.LoggingReceive
 import akka.routing.ActorRefRoutee
 import akka.routing.RoundRobinRoutingLogic
 import akka.routing.Router
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.DurationInt
 
 object PayloadBroadcast extends App {
   type PayloadId = Int
@@ -18,8 +20,11 @@ object PayloadBroadcast extends App {
   case class PayloadBroadcastMsg(jobId: Int, howManyConsumers: Int, payloads: Seq[Payload])
   case class PayloadAck(id: PayloadId)
   case class Done(jobId: Int)
-//  case class Payloads(payloads: Seq[Payload], chunkSize: Int)
+  case class Timeout(jobId: Int)
+  //  case class Payloads(payloads: Seq[Payload], chunkSize: Int)
+  case object TimeoutExpired
 
+  //  implicit val 
   class PayloadConsumer extends Actor with ActorLogging {
     def receive = LoggingReceive {
       case Payload(id, _) => sender ! PayloadAck(id)
@@ -32,32 +37,48 @@ object PayloadBroadcast extends App {
     def waiting: Receive = LoggingReceive {
       case PayloadBroadcastMsg(jobId, howManyConsumers, payloads) => {
         val consumers =
-          Vector.tabulate(howManyConsumers){ i =>
-          val consumer = context.actorOf(Props[PayloadConsumer], s"consumer$i-$jobId")
-          ActorRefRoutee(consumer)
-        }
+          Vector.tabulate(howManyConsumers) { i =>
+            val consumer = context.actorOf(Props[PayloadConsumer], s"consumer$i-$jobId")
+            ActorRefRoutee(consumer)
+          }
         val router = Router(RoundRobinRoutingLogic(), consumers)
         val payloadIds = payloads map (_.id)
         context.become(processing(jobId, payloadIds, sender))
 
-        payloads foreach(router.route(_, self))
+        payloads foreach (router.route(_, self))
       }
     }
 
-    def processing(jobId: Int, payloadIds: Seq[Int], ackActor: ActorRef): Receive = LoggingReceive {
+    def processing(jobId: Int, payloadIds: Seq[Int], ackActor: ActorRef,
+      timeout: FiniteDuration = 3.seconds): Receive = LoggingReceive
+    {
+      def becomeWaiting(msg: Any) = {
+        ackActor ! msg
+        context.children foreach context.stop
+        unstashAll()
+//        context.become(waiting)
+        context.unbecome
+      }
+
+      def scheduleTimeout = {
+        Option(system) foreach { system =>
+          import system.dispatcher // use the system's dispatcher as ExecutionContext
+          system.scheduler.scheduleOnce(timeout) { self ! TimeoutExpired }
+        }
+      }
+
       var idsToProcess: Seq[Int] = payloadIds
+
       def process: Receive = {
         case PayloadAck(id) => {
           idsToProcess = idsToProcess.diff(Seq(id))
-          if (idsToProcess.isEmpty) {
-            ackActor ! Done(jobId)
-            context.children foreach context.stop
-            unstashAll()
-            context.become(waiting)
-          }
+          if (idsToProcess.isEmpty) becomeWaiting(Done(jobId))
         }
+        case TimeoutExpired => becomeWaiting(Timeout(jobId))
         case _: PayloadBroadcastMsg => stash()
       }
+
+      scheduleTimeout
       process
     }
   }
@@ -68,15 +89,15 @@ object PayloadBroadcast extends App {
   val system = ActorSystem("PayloadBroadcast")
   val broadcaster = system.actorOf(Props[PayloadBroadcaster], "broadcaster")
   val sender = system.actorOf(Props(
-      new Actor {
-        def receive = LoggingReceive {
-          case Done(42) => {
-            println(s"shutting down system ...")
-            system.shutdown
-          }
+    new Actor {
+      def receive = LoggingReceive {
+        case Done(42) => {
+          println(s"shutting down system ...")
+          system.shutdown
         }
-      }), "terminator")
+      }
+    }), "terminator")
   broadcaster.tell(PayloadBroadcastMsg(42, 5, payloads(10)), sender)
 
-//  system.shutdown
+  //  system.shutdown
 }
